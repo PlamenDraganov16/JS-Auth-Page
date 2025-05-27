6ef4b326ccccca745f978cfad719b052d01895e8
Plamen Draganov <plamine95@gmail.com>
2025-05-27 15:08:16 +0300

    restructure
    made the .env file working

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'paolenaruseva', 
+  password: process.env.PASSWORD, 
   database: 'auth_app'
 });
 
